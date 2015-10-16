16600e87253df41c635a1016eb18b307bcf807a1
divlev <ivlev.dm@frtk.ru>
2015-10-16 13:05:01 +0300

    Added console input, new functions for MathVector

--- hometask_1/math_vector.cc
+++ hometask_1/math_vector.cc
@@ -8,13 +8,16 @@
 #include <sstream>
 #include <vector>
 
+MathVector::MathVector(): MathVector(0) {}
+
+
 MathVector::MathVector(unsigned int size): size_(size), values_(new double[size]) {}
 
 
-MathVector::MathVector(int argn, ...): size_(argn), values_(new double[argn]) { 
+MathVector::MathVector(int argn, ...): size_(argn), values_(new double[argn]) {
     va_list values;
     va_start(values, argn);
-    
+
     for (unsigned int i = 0; i < size_; ++i) {
         values_[i] = va_arg(values, double);
     }
@@ -22,11 +25,11 @@ MathVector::MathVector(int argn, ...): size_(argn), values_(new double[argn]) {
 }
 
 
-MathVector::MathVector(const MathVector& copy): size_(copy.size_), 
+MathVector::MathVector(const MathVector& copy): size_(copy.size_),
     values_(new double[size_]) {
-    
-    std::copy(copy.values_, copy.values_ + copy.size_, values_); 
-} 
+
+    std::copy(copy.values_, copy.values_ + copy.size_, values_);
+}
 
 
 MathVector::~MathVector() {
@@ -58,7 +61,7 @@ unsigned int MathVector::get_size() const {
 MathVector MathVector::opposite() const {
     MathVector opposite_vect = *this;
 
-    for ( unsigned int i = 0; i < size_; ++i) {
+    for (unsigned int i = 0; i < size_; ++i) {
         opposite_vect[i] *= -1;
     }
     return opposite_vect;
@@ -69,9 +72,8 @@ MathVector MathVector::add(const MathVector& other) const {
     if ( size_ != other.size_) {
         throw std::length_error("Vectors have different dimenisions");
     }
-    
-    MathVector sum = MathVector(size_);
 
+    MathVector sum = MathVector(size_);
 
     for (unsigned int i = 0; i < size_; ++i) {
         sum[i] = values_[i] + other.values_[i];
@@ -80,13 +82,18 @@ MathVector MathVector::add(const MathVector& other) const {
 }
 
 
+MathVector MathVector::sub(const MathVector& other) const {
+    return add(other.opposite());
+}
+
+
 double MathVector::scalar_mul(const MathVector& other) const {
-    if ( size_ != other.size_) {
+    if (size_ != other.size_) {
         throw std::length_error("Vectors have different dimenisions");
     }
-    
+
     double res = 0.0;
-    
+
     for (unsigned int i = 0; i < size_; ++i) {
         res += values_[i] * other.values_[i];
     }
@@ -108,15 +115,16 @@ MathVector& MathVector::operator=(MathVector copy) {
     std::swap(size_, copy.size_);
     std::swap(values_, copy.values_);
     return *this;
-} 
+}
 
 
 double& MathVector::operator[](unsigned int dim) {
-    if (dim >= size_) {
-        throw std::out_of_range("There is not such dimenision\n");
-    }
-    
     return values_[dim];
+}
+
+
+MathVector MathVector::operator-(const MathVector& other) const {
+    return sub(other);
 }
 
 
@@ -129,6 +137,14 @@ double MathVector::operator*(const MathVector& other) const {
     return scalar_mul(other);
 }
 
+
+MathVector operator*(double coef, const MathVector& vect) {
+    MathVector res = MathVector(vect.size_);
+    for (unsigned int i = 0; i < vect.size_; ++i) {
+        res[i] = vect.values_[i] * coef;
+    }
+    return res;
+}
 
 
 std::ostream& operator<<(std::ostream& os, const MathVector& vect) {
@@ -175,7 +191,7 @@ std::istream& operator>>(std::istream& is, MathVector& vect) {
         input += c;
         is >> c;
     }
-    
+
     std::vector<std::string> str_values = split(input, ',');
     double* buf = new double[str_values.size()];
     if (vect.size_ != 0) {
--- hometask_1/test.cpp
+++ hometask_1/test.cpp
@@ -4,11 +4,39 @@
 
 
 int main(int argc, char** argv) {
-    MathVector a(3, -11.0, 4.0, -5.0);
-    std::cout << a << std::endl;
-    MathVector b = a + a;
-    std::cout << b << std::endl;
-    std::cin >> b;
-    std::cout << b << std::endl;
-    return 0;
+    std::cout << "Input format: (vect) (+|-|*) (vect) or number * (vect)," << std::endl;
+    std::cout << "e.g. (-13.45, 15) + (0.45, -5), -13.45 * (56, 0)" << std::endl;
+    std::cout << "Enter q to exit" << std::endl;
+
+    char c;
+    MathVector a;
+    MathVector b;
+    double coef;
+
+    std::cin >> c;
+    while (c != 'q') {
+        std::cin.putback(c);
+        std::cout << ">> ";
+        if ( c != '(') { 
+            std::cin >> coef >> c >> a;
+            std::cout << coef * a;
+        } else {
+            std::cin >> a >> c >> b;
+            switch (c) {
+                case '+':
+                    std::cout << a + b;
+                    break;
+                case '-':
+                    std::cout << a - b;
+                    break;
+                case '*':
+                    std::cout << a * b;
+                    break;
+                default:
+                    std::cout << "Unknown operation";
+            }
+        }
+        std::cout << std::endl;
+        std::cin >> c;
+   }
 }
